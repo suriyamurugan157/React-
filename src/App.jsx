86f6f2a039f4336df472f6a4d88420196d07ec8e
suriyamurugan157 <suriyamurugan157@gmail.com>
2025-03-05 16:07:09 +0530

    My commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SignIn from "./Components/SignIn";
+
+
+function App() {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center w-[100vw] font-manrope"
+      style={{ backgroundImage: "url('/Image1.jpg')" }}
+    >
+      <div className="w-[28%] h-[500px] bg-white p-6 shadow-lg">
+        <h2 className="text-3xl font-semibold text-center text-purple-950 mb-[5%]">
+          Sign In
+        </h2>
+        <p className="text-center text-b mb-[20%] font-light">
+          Welcome, Sign-in to continue
+        </p>
+
+        <form className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Email *
+            </label>
+            <input
+              type="email"
+              className="w-full mt-1 p-3 border rounded-none focus:outline-none focus:ring-2 focus:ring-purple-500 font-normal"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Password *
+            </label>
+            <input
+              type="password"
+              className="w-full mt-1 p-3 border focus:outline-none focus:ring-2 focus:ring-purple-500 font-normal"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full rounded-none bg-gray-200 text-gray-400 py-3 square-md hover:bg-purple-700 transition"
+          >
+            Next
+          </button>
+
+          <p className="text-right text-sm text-red-500 cursor-pointer hover:underline mt-[3.5%]">
+            Forgot Password?
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
